d92bfd977062cf319a21f1685883abbf7dda01e3
wyr07 <aymeric@brochier.ch>
2017-03-29 15:08:47 +0200

    res zip complet + separe et propriete geodiff_type

--- web/Transfert.php
+++ web/Transfert.php
@@ -1,4 +1,3 @@
-
 <?php
 //phpinfo();
 if(isset($_POST['data']))
@@ -10,31 +9,23 @@ if(isset($_POST['data']))
 	$data1=$tab[0];
 	$data2=$tab[1];
 	$data3=$tab[2];
+	$data4=$tab[3];
 	#echo "$data1<br>$data2<br>$data3";
-
 	$uid = round(1000 * microtime(true));
-	$uid = "YYY";
-	$file = "/var/tmp/$uid.zip";#pb ici ???
-
+	$file = "/var/tmp/$uid.zip";
 	$zip = new ZipArchive();
-
 	$zip->open($file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 	$zip->addFromString('modif.json',$data1);
 	$zip->addFromString("ajout.json",$data2);
 	$zip->addFromString("suppression.json",$data3);
-	#$zip->addFromString("diff.json",$data4);
-
+	$zip->addFromString("diff.json",$data4);
 	$zip->close();
-
 	$size = filesize($file);
-
 	header('Content-type: application/zip');
 	header('Content-Disposition: attachment; filename='.$file);
 	header('Content-Length: '.$size);
 	header('Pragma: no-cache'); 
 	header('Expires: 0'); 
-
 	readfile($file);
 }
 ?>
-		
